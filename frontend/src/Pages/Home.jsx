6aa28db2d2652d7f4acf48db91fee9a02590f336
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-19 01:52:17 +0530

    added home background image

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import court3 from '../assets/images/court3.jpg'
 import court2 from '../assets/images/court2.jpg'
 import court4 from '../assets/images/court4.jpg'
 import player from '../assets/images/player.png'
+import home from '../assets/images/home.jpg'
 import icon1 from '../assets/images/icon01.png'
 import icon2 from '../assets/images/icon02.png'
 import icon3 from '../assets/images/icon03.png'
@@ -16,13 +17,13 @@ const Home = () => {
   return (
     <>
     {/* hero section */}
-    <section className='hero_section pt-[55px] 2xl:h-[800px]'>
-      <div className='container'>
+    <section className='hero_section pt-[55px] 2xl:h-[800px] bg-cover' style={{backgroundImage: `url(${home})`}}>
+      <div className='container' >
         <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
           {/* hero content */}
           <div>
           <div className='lg:w-[570px]'>
-            <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[55px] md:leading-[70px]'>
+            <h1 className='text-[36px] leading-[46px] text-white font-[800] md:text-[55px] md:leading-[70px]'>
             Super Easy Badminton Court Bookings Await!
             </h1>
             <p className='text_para'>
@@ -35,21 +36,21 @@ const Home = () => {
 
             <div>
               <h2 className='text-[36px] leading-[56px] lg:text-[30px] lg:leading-[54px] font-[700]
-               text-headingColor'>300+</h2>
+               text-white'>300+</h2>
                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
                <p className='text_para'>Courts</p>
             </div>
 
             <div>
               <h2 className='text-[36px] leading-[56px] lg:text-[30px] lg:leading-[54px] font-[700]
-               text-headingColor'>300+</h2>
+               text-white'>300+</h2>
                <span className='w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]'></span>
                <p className='text_para'>Users</p>
             </div>
 
             <div>
               <h2 className='text-[36px] leading-[56px] lg:text-[30px] lg:leading-[54px] font-[700]
-               text-headingColor'>100%</h2>
+               text-white'>100%</h2>
                <span className='w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]'></span>
                <p className='text_para'>Experience</p>
             </div>
@@ -58,11 +59,7 @@ const Home = () => {
           </div>
           </div>
 
-        <div className='flex gap-[30px] justify-end '>
-          <div>
-            <img className='w-full' src={court4} alt=""/>
-          </div>
-        </div>
+        
 
         </div>
 
