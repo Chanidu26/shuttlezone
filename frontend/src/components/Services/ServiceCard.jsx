3625cfcb64e8dffff7c10a183439994a7d8d6a4a
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-19 02:11:43 +0530

    added shadow hover effect

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -4,7 +4,7 @@ const ServiceCard = ({item,index}) => {
 
     const {name, desc, icon, bgColor, textColor} = item
   return (
-    <div className='py-[10px] px-3 lg:px-5 hover:scale-105 transition-all duration-300'>
+    <div className='py-[10px] px-3 lg:px-5 hover:scale-105 hover:shadow-lg transition-all duration-300'>
        <h2 className='text-[23px] leading-9 text-headingColor font-[700]'>{name} {icon}</h2>
        <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>{desc}</p>
     </div>
