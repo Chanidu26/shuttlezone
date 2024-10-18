7ec79fad6d5bead4833743e3add375d4274955a3
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-18 12:53:26 +0530

    added review model

---
+++ backend/models/Review.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    court: {
+      type: mongoose.Types.ObjectId,
+      ref: "Court",
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    reviewText: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Review", ReviewSchema);
