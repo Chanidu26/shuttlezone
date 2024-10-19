fc3415f8c0ae46c98058b8a36d1c33b889bc884a
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-10-19 14:24:26 +0530

    configure base url

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const Login = () => {
   const [email,setEmail] = useState('')
